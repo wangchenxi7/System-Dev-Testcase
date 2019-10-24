55192189644f545210a5cf561f4f8cc9052720dc
Chenxi Wang <wangchenxi@g.ucla.edu>
2019-10-24 11:24:34 -0700

    Add IB device info check

---
+++ c-test/struct_size.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+
+int main(int argc, char* argv[]){
+
+
+	printf("byte size of \n unsigned long : %d, \n unsigned int : %d, \n  long : %d, \n", 
+							sizeof(unsigned long), sizeof(unsigned int), sizeof(long));
+
+
+	return 0;
+}
