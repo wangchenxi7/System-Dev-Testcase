d7f7107c73b2147e209dcad2b841a813ee090388
zion-5 <909475573@qq.com>
2020-11-18 00:48:01 -0800

    merge master

--- jvmti/ClassInstrumentWithRescribo/do_instrument.sh
+++ jvmti/ClassInstrumentWithRescribo/do_instrument.sh
@@ -20,12 +20,9 @@ fi
 # Options
 
 # Log 
-<<<<<<< HEAD
 LogInfo="-Xmx128M -Xms128M -XX:PrefetchThreads=1 -XX:PrefetchNum=100 -XX:PrefetchSize=1000 -XX:PrefetchQueueThreshold=100 -XX:+PrintGCDetails"
 #-Xlog:prefetch=debug
-=======
 #LogInfo="-Xlog:prefetch=debug"
->>>>>>> master
 
 LD_LIBRARY_PATH=`pwd`:${LD_LIBRARY_PATH} gdb --args /mnt/ssd/haoran/jdk12u-dev/build/linux-x86_64-server-release/jdk/bin/java ${LogInfo} -agentlib:${JVMTILib} ${JavaApp}
 # LD_LIBRARY_PATH=`pwd`:${LD_LIBRARY_PATH} /mnt/ssd/haoran/jdk12u-dev/build/linux-x86_64-server-slowdebug/jdk/bin/java -agentlib:${JVMTILib} -Xmx128M -Xms128M -XX:PrefetchThreads=1 -XX:PrefetchNum=100 -XX:PrefetchSize=1000 -XX:PrefetchQueueThreshold=100 -XX:+PrintGCDetails Simple
