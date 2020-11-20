769b0fe002a6196c2652b1e5f393e3593aab5142
zion-5 <909475573@qq.com>
2020-11-19 22:51:15 -0800

    change do instrucment

--- jvmti/ClassInstrumentWithRescribo/do_instrument.sh
+++ jvmti/ClassInstrumentWithRescribo/do_instrument.sh
@@ -24,5 +24,5 @@ LogInfo="-Xmx128M -Xms128M -XX:PrefetchThreads=1 -XX:PrefetchNum=100 -XX:Prefetc
 #-Xlog:prefetch=debug
 #LogInfo="-Xlog:prefetch=debug"
 
-LD_LIBRARY_PATH=`pwd`:${LD_LIBRARY_PATH} gdb --args /mnt/ssd/haoran/jdk12u-dev/build/linux-x86_64-server-release/jdk/bin/java ${LogInfo} -agentlib:${JVMTILib} ${JavaApp}
+LD_LIBRARY_PATH=`pwd`:${LD_LIBRARY_PATH} gdb --args /mnt/ssd/haoran/jdk12u-dev/build/linux-x86_64-server-slowdebug/jdk/bin/java ${LogInfo} -agentlib:${JVMTILib} ${JavaApp}
 # LD_LIBRARY_PATH=`pwd`:${LD_LIBRARY_PATH} /mnt/ssd/haoran/jdk12u-dev/build/linux-x86_64-server-slowdebug/jdk/bin/java -agentlib:${JVMTILib} -Xmx128M -Xms128M -XX:PrefetchThreads=1 -XX:PrefetchNum=100 -XX:PrefetchSize=1000 -XX:PrefetchQueueThreshold=100 -XX:+PrintGCDetails Simple
