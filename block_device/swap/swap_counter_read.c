ca82b318c311cea74b4a61c98ec4b28ef84d0171
Chenxi Wang <wangchenxi@g.ucla.edu>
2021-01-28 22:58:47 -0800

    add new files

---
+++ block_device/swap/swap_counter_read.c
@@ -0,0 +1,33 @@
+
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <sys/mman.h>  
+#include <sys/errno.h>
+#include </usr/include/asm-generic/mman-common.h>
+
+#include "stdint.h"
+#include "stdio.h"
+
+
+
+#define SYS_ON_DEMAND_SWAPIN		337
+
+
+
+int main(){
+				
+	int type = 0x1;
+	uint64_t request_addr 	= 0x40000000; // start of RDMA meta space, 1GB not exceed the swap partitio size.
+	uint64_t size  					=	0x40000000;		// 1GB data
+
+	printf("	#1 Check current swapped out pages num\n");
+	size_t on_demand_swapin = syscall(SYS_ON_DEMAND_SWAPIN);
+	printf("	#1 on-demand swap-in pages num 0x%llx \n", on_demand_swapin);  // should be 0.
+
+
+
+
+  return 0;
+
+}
